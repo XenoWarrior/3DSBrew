26165f51d2672025a383dbda0b5e8e38ee33532b
Ashley Scott <compaqtomania@live.co.uk>
2015-02-28 20:55:14 +0000

    Fixed program and commented more code.

--- source/main.c
+++ source/main.c
@@ -18,9 +18,9 @@ Result httpDownload(httpcContext *httpContext)
 	u8* frameBufferTop;				// The buffer for rendering smea's image on top-screen
 	u32 statusCode = 0;				// HTTP status code returned from httpcGetResponseStatusCode
 	u32 rawSize=0, contentSize=0;	// Sets the rawSize (dimensions of file) and contentSize (total bytes)
-	char *ufSize = "";				// User friendly size conversion
-	char *sizeType = "KB";			// Set size to KB
-	u32 convertedSize = 0;			// User friendly size conversion (actual value)
+	//char *ufSize = "";				// User friendly size conversion
+	//char *sizeType = "KB";			// Set size to KB
+	//u32 convertedSize = 0;			// User friendly size conversion (actual value)
 	u8 *buf;						// Uses malloc to allocate space to the memory for image
 
 	returnCode = httpcBeginRequest(httpContext);							// Start the HTTP request
@@ -37,14 +37,15 @@ Result httpDownload(httpcContext *httpContext)
 	if(returnCode != 0)														// Same as above to return errors
 		return returnCode;
 
-	// UNFINISHED!
-	// TODO: CONVERT SIZE TO STRING AND APPEND TO STRING FOR USER FRIENDLY OUTPUT!
-	convertedSize = contentSize / 100;
-	ufSize = malloc(strlen(convertedSize) + strlen(sizeType));
-	strcpy(ufSize, convertedSize);
-	strcpy(ufSize, sizeType);
-
-	printf("[<--] Size: %s\n", ufSize);										// Output size to screen
+	/* UNFINISHED!
+		// TODO: CONVERT SIZE TO STRING AND APPEND TO STRING FOR USER FRIENDLY OUTPUT!
+		convertedSize = contentSize / 100;
+		ufSize = malloc(strlen(convertedSize) + strlen(sizeType));
+		strcpy(ufSize, convertedSize);
+		strcpy(ufSize, sizeType);
+	*/
+	
+	printf("[<--] Size: %i\n", rawSize);									// Output size to screen
 	gfxFlushBuffers();														// Flush graphics buffer
 
 	buf = (u8*)malloc(contentSize);											// Allocate memory for the image
@@ -96,26 +97,27 @@ Result httpDownload(httpcContext *httpContext)
 *****************************************************************/
 int main()
 {
-	bool use3D = false;
+	bool use3D = false;		// Set the bool to false to disable 3D. Set in gfxSet3D();
 
-	gfxInitDefault();
-	gfxSet3D(use3D);
-	httpcInit();
+	gfxInitDefault();		// Start graphics controller
+	gfxSet3D(use3D);		// Start 3D controller
+	httpcInit();			// Start HTTP controller
 
+	// Start console on bottom screen with a message containing various features
 	consoleInit(GFX_BOTTOM, NULL);
-	printf("[CMD] Started console on bottom screen.\n This testing application implements a few features such as:\n  Downloading files (Press A)\n   Starting up 3D mode (Press Select)\n   Rendering graphics (Press B)\n   Closing application (Press Start)\n");
+	printf("[CMD] Started console on bottom screen.\n This testing homebrew app!\n  Downloading files (Press A)\n   Starting up 3D mode (Press Select)\n   Rendering graphics (Press B)\n   Closing application (Press Start)\n");
 
 	// Main loop
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
-		hidScanInput();
-		u32 kDown = hidKeysDown();
+		hidScanInput();					// Scanning the input
+		u32 kDown = hidKeysDown();		// Check if input pressed
 
+		// Will check if B is being pressed
 		if(kDown & KEY_B)
 		{
-			// Example rendering code that displays a white pixel
-			// Please note that the 3DS screens are sideways (thus 240x400 and 240x320)
+			// EXAMPLE CODE TO DRAW DOT ON SCREEN
 			u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
 			memset(fb, 0, 240*400*3);
 			fb[3*(10+10*240)] = 0xFF;
@@ -123,59 +125,73 @@ int main()
 			fb[3*(10+10*240)+2] = 0xFF;
 		}
 
+		// Will check if A is being pressed
 		if(kDown & KEY_A)
 		{
-			char *webUrl = "http://devkitpro.org/misc/httpexample_rawimg.rgb";
-			Result retResult = 0;
-			httpcContext httpContext;
-
-			gfxFlushBuffers();
+			char *webUrl = "http://devkitpro.org/misc/httpexample_rawimg.rgb";	// The download URL
+			Result retResult = 0;												// Result returned from various calls
+			httpcContext httpContext;											// The HTTP handler
+			gfxFlushBuffers(); 													// Flush buffers
 
 			printf("[---] Attempting to initiate download...\n[-->] Connecting to:\n\n%s\n\n", webUrl);
 
-			retResult = httpcOpenContext(&httpContext, webUrl , 0);
-			gfxFlushBuffers();
+
+			retResult = httpcOpenContext(&httpContext, webUrl , 0);		// Open webrequest
+			gfxFlushBuffers();											// Flush buffers
 
 			printf("[DBG] retResult value is: %"PRId32"\n\n", retResult);
 
+			// Will check if there were any issues with download
 			if(retResult == 0)
 			{
 				printf("[!!!] Connected!\n[<--] Downloading files...\n");
 				
+				// Start download from server using the setup handler
 				retResult = httpDownload(&httpContext);
 
+				// Check for any errors with download
 				if(retResult == 0)
 				{
 					printf("[DBG] retResult value is: %"PRId32"\n[-X>] Closing web request...\n", retResult);
+
+					// Close the HTTP request
 					httpcCloseContext(&httpContext);
 
 					printf("[!!!] Done!\n");
 				}
 				else
 				{
+					// Printout error message
 					printf("[!!!] Status code returned: %"PRId32, retResult);
 				}
 			}
 			else
 			{
+				// Printout error message
 				printf("[!!!] Error: was not able to connect to server!\n");
 			}
+			// Flush the buffers
 			gfxFlushBuffers();
 		}
 
+		// Will check if Select is being pressed
 		if(kDown & KEY_SELECT)
 		{
+			// Simply to switch between on or off
 			if(use3D)
-				use3D = false;
+				use3D = false;		// If using 3D (true), set to false
 			else
-				use3D = true;
+				use3D = true;		// If not using 3D (false), set to true
 
 			printf("[3D ] Setting 3D feature to %i\n", use3D);
-			gfxSet3D(use3D);
+			gfxSet3D(use3D);		// Finally apply the state to 3D.
 		}
 
+		// Will check if start is being pressed
 		if (kDown & KEY_START)
 		{
+			// Exit the program
+			// Breaking out of the loop finally exits the services and dumps back to HB menu
 			printf("[!!!] Exiting program...\n");
 			break;
 		}
@@ -185,6 +201,7 @@ int main()
 		gfxSwapBuffers();
 	}
 
+	httpcExit();
 	gfxExit();
 	return 0;
 }
